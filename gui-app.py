0942886973b724b1d483ca804d9f013364ef20a4
Adam Simo <adamsimo626@gmail.com>
2023-10-19 22:58:52 +0200

    create input for cups

--- gui-app.py
+++ gui-app.py
@@ -1,7 +1,7 @@
 from database import food_list
 from tkinter import *
 from tkinter import ttk
-import sys
+# import sys
 
 # creating the root window and config
 root = Tk() # main window
@@ -14,10 +14,12 @@ root.geometry("310x410+700+150")
 # GUI elements
 text = Label(root, text="Cups to grams converter",font=(30), 
              anchor='n',padx=5, pady=5)
-text2 = Label(root, text='Select the ingredient')
+text2 = Label(root, text='1. Select the ingredient')
 ingredient_choser = Listbox(root, selectmode=SINGLE, width=24)
+text3 = Label(root, text='2. Input number of Cups',pady=3)
+input_value = Text(root, width=5, height=1, pady=3)
 convert_value = Button(root, text='Convert', command= lambda: converter(1)
-                       ) # prints out only first selection :-( 
+                       ) 
 output_text = Text(root, width=24, height=3)
 
 
@@ -29,6 +31,7 @@ for a in food_list:
 def converter(cups):
     """Conversion of cups to grams"""
     ingredient = ingredient_choser.get(ingredient_choser.curselection()) # get selected item from list box 
+    cups = input_value.get(1.0, "end-1c") # 
     gram_value = float(cups) * float(food_list[ingredient])
     print(gram_value)
 
@@ -37,6 +40,8 @@ def converter(cups):
 text.pack()
 text2.pack()
 ingredient_choser.pack()
+text3.pack()
+input_value.pack()
 convert_value.pack(anchor='center')
 output_text.pack()
 
